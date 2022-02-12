5644ff00f43f6094e94972cf7ed7b5188525f39f
norbertbraun <bnorbertjs@gmail.com>
2022-02-12 14:24:42 +0100

    init

---
+++ src/Rudus.js
@@ -0,0 +1,13 @@
+// React
+export class Rudus {
+  static createElement() {
+    // TODO: Implement me
+  }
+}
+
+// ReactDOM
+export class RudusDOM {
+  static render() {
+    // TODO: Implement me
+  }
+}
