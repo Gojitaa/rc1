98b4e8e9d80c3f3073b6b76be9730e68d32a269f
norbertbraun <bnorbertjs@gmail.com>
2022-02-12 15:37:55 +0100

    lifecycle methods

--- src/index.js
+++ src/index.js
@@ -1,14 +1,56 @@
-import { Rudus, RudusDOM } from './Rudus'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './index.css';
 
-/** @jsxRuntime classic */
-/** @jsx Rudus.createElement */
-const App = <div id='container'>
-              <div>
-                <a href='/'>Hello Rudus</a>
-              </div>
-            </div>
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('List constructed')
+    this.state = {
+      listId: '123f-6789-3421'
+    }
+  }
+
+  componentDidMount() {
+    console.log('List mount')
+
+    this.setState({ listId: '456f-6789-3421' })
+  }
+
+  shouldComponentUpdate(previousProps, previousState){
+    console.log('List shouldUpdate?')
+
+    return this.props.items !== previousProps.items
+  }
+
+  render(){
+    console.log('List render')
+    return <ul>{
+      this.props.items.map(item => <li key={item}>{item}</li>)
+    }</ul>
+  }
+}
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      items: ['Item1', 'Item2', 'Item3', 'Item4', 'Item5', 'Item6', 'Item7']
+    };
+  }
+
+  componentDidMount(){
+    console.log('App mount')
+  }
+
+  render(){
+    console.log('App render')
+    return <div id='App'>
+      <List items={this.state.items}/>
+    </div>
+  }
+}
 
 const root = document.getElementById('root')
 
-RudusDOM.render(App, root);
+ReactDOM.render(<App />, root);
